ccca50cccf548964575490aa3eb40c403f522af8
Piron-Johny <piron.johny@gmail.comm>
2021-11-13 14:25:05 +0200

    Начало работы. Не работает

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox/dist/simple-lightbox';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+function createHTMLMarkup(elem) {
+  galleryEl.insertAdjacentHTML(
+    'beforeend',
+    `
+      <a class="gallery__item" href="${elem.original}">
+        <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
+      </a>
+    `,
+  );
+}
+
+const genGalleryItem = galleryItems => {
+  galleryItems.map(elem => createHTMLMarkup(elem));
+};
+genGalleryItem(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlay: true,
+  preloading: true,
+  alertErrorMessage: 'Изображение не найдено, будет загружено следующее изображение',
+});
