2fb26b3dabf38536c540b1eb4cb80bddc73acfa3
Piron Evgeniy <piron.johny@gmail.com>
2021-11-15 09:54:05 +0000

    task 2 - start

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const options = {
+  id: 59777392,
+  width: 640,
+  loop: true
+};
+
+const player = new Vimeo.Player('vimeo-player', options);
+
+player.setVolume(0);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
