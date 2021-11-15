491e64b56b8e4e84a2790f9b723e6f7ba5fa016e
Piron Evgeniy <piron.johny@gmail.com>
2021-11-15 09:21:09 +0000

    task 1 - finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
-// Add imports above this line
+
+import SimpleLightbox from "simplelightbox";
 import { galleryItems } from './gallery-items';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
 const galleryEl = document.querySelector('.gallery');
@@ -22,7 +22,7 @@ const genGalleryItem = galleryItems => {
 };
 genGalleryItem(galleryItems);
 
-const lightbox = new SimpleLightbox('.gallery a', {
+let lightbox = new SimpleLightbox('.gallery a', {
   captions: true,
   captionsData: 'alt',
   captionDelay: 250,
