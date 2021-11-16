f89f936732997579cf4215df72deba638853cdf5
Piron Evgeniy <piron.johny@gmail.com>
2021-11-16 13:55:15 +0000

    Connected setCurrentTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,24 +8,26 @@ const player = new Player('vimeo-player', {
 });
 
 player.on('timeupdate', (data) => {
-  console.log(data);
   localStorage.setItem("videoplayer-current-time", data.seconds)
 });
 
 const currentTime = localStorage.getItem("videoplayer-current-time");
 
-player.setCurrentTime(currentTime).then(function (seconds) {
-  // seconds = the actual time that the player seeked to / фактическое время, которое искал игрок
-  console.log('sec', seconds);
-}).catch(function (error) {
-  switch (error.name) {
-    case 'RangeError':
-      // the time was less than 0 or greater than the video’s duration / ремя было меньше 0 или больше, чем продолжительность видео 
-      break;
+player.setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to / фактическое время, которое искал игрок
+    console.log('sec', seconds);
+  }).catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration / ремя было меньше 0 или больше, чем продолжительность видео 
+        break;
 
-    default:
-      // some other error occurred / произошла другая ошибка
-      break;
-  }
-});
+      default:
+        // some other error occurred / произошла другая ошибка
+        break;
+    }
+  });
+
+
 
