4e07c150c080983bfb97b3a0c80a660389e74d77
Piron Evgeniy <piron.johny@gmail.com>
2021-11-16 09:43:57 +0000

    Task 2. Финишная прямая

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,25 +1,31 @@
 import Player from '@vimeo/player';
+// import { save, load, remove } from './storage';
+
 
 const player = new Player('vimeo-player', {
   id: 19231868,
   width: 640
 });
 
-// player.on('play', function () {
-//   console.log('played the video!');
-// });
-
-// const data = {
-//   duration: 61.857,
-//   percent: 0.502,
-//   seconds: 31.052,
-// }
-
-player.on('play', (data)=> {
+player.on('timeupdate', (data) => {
   console.log(data);
+  localStorage.setItem("videoplayer-current-time", data.seconds)
+});
 
-} );
+const currentTime = localStorage.getItem("videoplayer-current-time");
 
+player.setCurrentTime(currentTime).then(function (seconds) {
+  // seconds = the actual time that the player seeked to / фактическое время, которое искал игрок
+  console.log('sec', seconds);
+}).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      // the time was less than 0 or greater than the video’s duration / ремя было меньше 0 или больше, чем продолжительность видео 
+      break;
 
-
+    default:
+      // some other error occurred / произошла другая ошибка
+      break;
+  }
+});
 
