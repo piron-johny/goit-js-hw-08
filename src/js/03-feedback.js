5aabdec68bcf9d9c4db10aea544440c568d12da0
Piron Evgeniy <piron.johny@gmail.com>
2021-11-19 09:55:16 +0000

    bugfix task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -7,11 +7,16 @@ const inputPassword = formEl.querySelector('[name=message]');
 formEl.addEventListener('input', throttle(onInputForm, 500));
 formEl.addEventListener('submit', onSubmitForm);
 
-const setData = {};
-const getData = localStorage.getItem("feedback-form-state");
+const LOCAL_STORAGE_KEY = "feedback-form-state";
+const data = {
+  email: '',
+  password: '',
+};
+
+let getData = localStorage.getItem(LOCAL_STORAGE_KEY);
 
 (() => {
-  if (localStorage.getItem("feedback-form-state") !== null) {
+  if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
     inputEmail.value = JSON.parse(getData).email;
     inputPassword.value = JSON.parse(getData).password;
   }
@@ -19,19 +24,31 @@ const getData = localStorage.getItem("feedback-form-state");
 
 function onInputForm(e) {
   if (e.target.nodeName === 'INPUT') {
-    setData.email = e.target.value
+    data.email = e.target.value
   }
   if (e.target.nodeName === 'TEXTAREA') {
-    setData.password = e.target.value;
+    data.password = e.target.value;
   }
-
-  localStorage.setItem("feedback-form-state", JSON.stringify(setData))
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(data))
 };
 
 function onSubmitForm(e) {
   e.preventDefault()
-  console.log(JSON.parse(getData));
+
+  if (JSON.parse(getData)) {
+    console.log(JSON.parse(getData));
+  } else {
+    console.log(data);
+  }
+
+  data.email = '';
+  data.password = '';
   e.currentTarget.reset()
-  localStorage.removeItem("feedback-form-state")
+  localStorage.removeItem(LOCAL_STORAGE_KEY)
+
+  getData = localStorage.getItem(LOCAL_STORAGE_KEY);
+
 };
+
+
 
